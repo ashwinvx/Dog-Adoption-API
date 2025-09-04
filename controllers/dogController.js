ffb7ae093e78feed237049a1ffd13332b64f6f55
Ashwin <ashwinvx@users.noreply.github.com>
2025-09-04 14:20:23 -0400

    added pagination and filter by category

--- controllers/dogController.js
+++ controllers/dogController.js
@@ -28,7 +28,7 @@ module.exports.dogsid_get = async (req, res) => {
 
 module.exports.adoptdogid_post = async (req, res) => {
     const dog = await Dog.findById(req.params.id);
-    if (dog) {
+    if (dog && dog.adoptionStatus !== 'adopted' && !dog.ownerInfo.equals(req.userId)) {
         dog.adoptionStatus = 'adopted';
         dog.adoptedOwnerInfo = req.userId;
         const updateddog = await dog.save();
@@ -38,28 +38,49 @@ module.exports.adoptdogid_post = async (req, res) => {
 }
 
 module.exports.removedogid_post = async (req, res) => {
-    console.log('dog id-->', req.url);
-    const deletedDog = await Dog.findByIdAndDelete(req.params.id);
-    if (!deletedDog) {
-        console.log('dog not found');
-        return null;
+    const dog = await Dog.findById(req.params.id);
+    console.log('dog owner-->', dog.ownerInfo);
+    console.log('logged in user-->', req.userId);
+    if (dog && dog.adoptionStatus !== 'adopted' && dog.ownerInfo.equals(req.userId)) {
+        const result = await Dog.deleteOne({ _id: dog._id });
+        if (result.deletedCount > 0) {
+            console.log('dog successfully removed', result);
+            res.status(201).json({ dog: 'deleted' });
+        } else {
+            console.log('dog not found');
+            return null;
+        }
     }
-    console.log('dog successfully removed', deletedDog);
-    res.status(201).json({ dog: deletedDog });
 }
 
 module.exports.mydogs_get = async (req, res) => {
-    const filter = req.query.dogs;
-    console.log('filter-->', filter);
-    let dogs;
-    if (filter == 'registered') {
-        dogs = await Dog.find({ ownerInfo: req.userId });
-    }
-    else if (filter == 'adopted') {
-        dogs = await Dog.find({ adoptedOwnerInfo: req.userId });
-    }
-    if (dogs) {
-        console.log('mydogs-->', dogs);
-        res.render('mydogs', { title: 'My dogs', dogs: dogs });
+    const page = parseInt(req.query.page) || 1; // Default to page 1
+    const limit = parseInt(req.query.limit) || 10; // Default to 10 items per page
+    const category = req.query.category;
+    const skip = (page - 1) * limit; // Calculate documents to skip for pagination
+    console.log('category-->', category);
+    try {
+        let dogs, title, totalDogs, totalPages;
+        if (category == 'registered') {
+            dogs = await Dog.find({ ownerInfo: req.userId })
+                .skip(skip)
+                .limit(limit);
+            totalDogs = await Dog.countDocuments({ ownerInfo: req.userId });
+            title = 'My dogs';
+        }
+        else if (category == 'adopted') {
+            dogs = await Dog.find({ adoptedOwnerInfo: req.userId })
+                .skip(skip)
+                .limit(limit);
+            totalDogs = await Dog.countDocuments({ adoptedOwnerInfo: req.userId });
+            title = 'My adopted dogs';
+        }
+        totalPages = Math.ceil(totalDogs / limit);
+        if (dogs) {
+            console.log('mydogs-->', dogs);
+            res.render('mydogs', { title, dogs });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 }
